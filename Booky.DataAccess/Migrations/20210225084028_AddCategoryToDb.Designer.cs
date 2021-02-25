ff7b97151b32bdaa14705fd28930c777cca04b1f
LordAldi <aldianugra09@gmail.com>
2021-02-25 15:42:01 +0700

    add category to db

--- Booky.DataAccess/Migrations/20210225084028_AddCategoryToDb.Designer.cs
+++ Booky.DataAccess/Migrations/20210225084028_AddCategoryToDb.Designer.cs
@@ -4,14 +4,16 @@ using Booky.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Booky.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210225084028_AddCategoryToDb")]
+    partial class AddCategoryToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
