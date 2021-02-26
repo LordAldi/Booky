397d9727dc0dfea01676e9514621e3a40783c994
LordAldi <aldianugra09@gmail.com>
2021-02-26 16:49:07 +0700

    add CRUD for CoverType

---
+++ Booky.DataAccess/Repository/CoverTypeRepository.cs
@@ -0,0 +1,29 @@
+﻿using Booky.DataAccess.Data;
+using Booky.DataAccess.Repository.IRepository;
+using Booky.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booky.DataAccess.Repository
+{
+    public class CoverTypeRepository:Repository<CoverType>,ICoverTypeRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CoverTypeRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(CoverType coverType)
+        {
+            var objFromDb = _db.CoverTypes.FirstOrDefault(s => s.Id == coverType.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = coverType.Name;
+            }
+        }
+    }
+}
