8456c41ff05b357a41ed7e7591effdb11bed974a
LordAldi <aldianugra09@gmail.com>
2021-02-26 11:32:09 +0700

    create categoryRepository

---
+++ Booky.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,31 @@
+﻿using Booky.DataAccess.Data;
+using Booky.DataAccess.Repository.IRepository;
+using Booky.Models;
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Booky.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryRepository(ApplicationDbContext db): base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var objFromDb = _db.Categories.FirstOrDefault(s => s.Id == category.Id);
+            if(objFromDb != null)
+            {
+                objFromDb.Name = category.Name;
+                _db.SaveChanges();
+            }
+            
+        }
+    }
+}
